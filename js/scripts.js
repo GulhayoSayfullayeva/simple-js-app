ec178544ba0e0559eeba56b0d9e1d30924b26ea9
GulhayoSayfullayeva <gulhayo.sayfullayeva@gmail.com>
2023-02-08 17:08:56 +0100

    Javascript objects
    Pokemon list created and some items are added.

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,8 @@
-let favoriteFood = "pizza";
-
-
-document.write(favoriteFood);
-alert("Hello World!");
+let pokemonList = [];
+pokemonList =[
+    { name: "Bulbasaur", height: 1, types: ["grass", "poison"]},
+    { name: "Venusaur", height: 2, types: ["monster", "grass"]},
+    { name: "Charmander", height: 0.6, types: ["monster", "dragon"]},
+    { name: "Charmeleon", height: 1.1, types: ["monster", "dragon"]},
+    { name: "Charizard", height: 1.7, types: ["monster", "dragon"]}
+]
