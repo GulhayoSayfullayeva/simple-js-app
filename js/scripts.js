d8bf1c4489a1aee8fc88c02744fdfefa26d6b893
GulhayoSayfullayeva <gulhayo.sayfullayeva@gmail.com>
2023-02-11 00:00:02 +0100

    PokemonRepository as in IIFE
    add, getAll functions are added to pokemonList, their contents are checked, their keys also are checked.

--- js/scripts.js
+++ js/scripts.js
@@ -1,19 +1,48 @@
-let pokemonList = [];
-let pokemon = {};
-pokemonList =[
-    { name: "Bulbasaur", height: 1, types: ["grass", "poison"]},
-    { name: "Venusaur", height: 2, types: ["monster", "grass"]},
-    { name: "Charmander", height: 0.6, types: ["monster", "dragon"]},
-    { name: "Charmeleon", height: 1.1, types: ["monster", "dragon"]},
-    { name: "Charizard", height: 1.7, types: ["monster", "dragon"]}
-];
+let keys = ["name", "height", "types"];
 
-for( i=0; i<pokemonList.length; i++){
-     pokemon = pokemonList[i];
+let pokemonRepository = (function(){
+    pokemonList = [];
+    function add(pokemon){
+        pokemonList.push(pokemon);
+    }
+    function getAll(){
+        return pokemonList;
+    }
+    function addv(pokemon){
+        if( pokemon instanceof Object &&
+            pokemon.name instanceof String &&
+            pokemon.height instanceof Number &&
+            Array.isArray(pokemon.types)
+          ){
+            pokemonList.push(pokemon);
+          }
+        else{
+            console.log("Not valid data");
+        }  
+    }
+    return{
+         add: add,
+         getAll: getAll,
+         addv: addv
+        };
+})();
+pokemonRepository.add({name: "Bulbasaur", height: 1, types: ["grass", "poison"]});
+pokemonRepository.add({name: "Venusaur", height: 2, types: ["monster", "grass"]});
+pokemonRepository.add({name: "Charmander", height: 0.6, types: ["monster", "dragon"]});
+pokemonRepository.addv({name: 3, height: 0.6, types: ["monster", "dragon"]});
+pokemonRepository.getAll().forEach( function(pokemon){
      document.write('<div class="grid-item">');
      document.write(pokemon.name + "(height: " + pokemon.height + ")");
      if( pokemon.height >=2 ){
         document.write("- Wow, that’s big!");
      }
      document.write('</div>');
-}
+});
+
+pokemonRepository.getAll().forEach(function(pokemon){
+    if(JSON.stringify(Object.keys(pokemon)) === JSON.stringify(keys)){
+        console.log("Keys are as expected");
+    }
+});
+
+
