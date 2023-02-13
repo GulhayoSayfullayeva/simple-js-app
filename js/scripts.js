4408444d989427bc98794d1dbe71a247798c0114
GulhayoSayfullayeva <gulhayo.sayfullayeva@gmail.com>
2023-02-13 23:59:15 +0100

    DOM manipulation Javascript
    Pokemon list are created, event listeners are handled, addListItem and showDetails functions are added

--- js/scripts.js
+++ js/scripts.js
@@ -20,29 +20,41 @@ let pokemonRepository = (function(){
             console.log("Not valid data");
         }  
     }
+    function addListItem(pokemon){
+        let pokemonLists = document.querySelector("ul");
+        let pokemonItem = document.createElement("li");
+        let pokemonButton = document.createElement("button");
+        pokemonButton.innerText = pokemon.name;
+        pokemonButton.classList.add("pokemonButton");
+        pokemonItem.appendChild(pokemonButton);
+        pokemonLists.appendChild(pokemonItem);
+        pokemonButton.addEventListener('click', function(event){
+            showDetails(pokemon);
+        });
+    }
+    function showDetails(pokemon){
+        console.log(pokemon);
+    }
     return{
          add: add,
          getAll: getAll,
-         addv: addv
+         addv: addv,
+         addListItem: addListItem,
+         showDetails: showDetails
         };
 })();
 pokemonRepository.add({name: "Bulbasaur", height: 1, types: ["grass", "poison"]});
+pokemonRepository.add({name: "Metapod", height: 1, types: ["grass", "poison"]});
+pokemonRepository.add({name: "Weedle", height: 1, types: ["grass", "poison"]});
+pokemonRepository.add({name: "Squirtle", height: 1, types: ["grass", "poison"]});
+pokemonRepository.add({name: "Butterfree", height: 1, types: ["grass", "poison"]});
 pokemonRepository.add({name: "Venusaur", height: 2, types: ["monster", "grass"]});
 pokemonRepository.add({name: "Charmander", height: 0.6, types: ["monster", "dragon"]});
 pokemonRepository.addv({name: 3, height: 0.6, types: ["monster", "dragon"]});
 pokemonRepository.getAll().forEach( function(pokemon){
-     document.write('<div class="grid-item">');
-     document.write(pokemon.name + "(height: " + pokemon.height + ")");
-     if( pokemon.height >=2 ){
-        document.write("- Wow, that’s big!");
-     }
-     document.write('</div>');
+     pokemonRepository.addListItem(pokemon);
 });
 
-pokemonRepository.getAll().forEach(function(pokemon){
-    if(JSON.stringify(Object.keys(pokemon)) === JSON.stringify(keys)){
-        console.log("Keys are as expected");
-    }
-});
+
 
 
