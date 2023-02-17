e5c432418d11902adcb4303cae9c33b58f604dbd
GulhayoSayfullayeva <gulhayo.sayfullayeva@gmail.com>
2023-02-17 14:32:43 +0100

    Creating UI patterns with Javascript
    In order to display the details of pokemon, modals are created, it describes the name, image, height, types of selected pokemon; by clicking the x button, pressing ESC, clicking the outside of modal results closing the modal. README file is also updated.

---
+++ README.md
@@ -0,0 +1,13 @@
+Pokemon (First Project at CareerFoundry)
+
+This is a Javascript app, which uses an external API to get 150 pokemons from external url.
+
+How to use the site:
+When entering the main page of the Pokemon, there is the list of pokemons loaded from external API, you can click on every pokemon object in order to see the details of them, which opens the modal, which describes the name, the image, height and types of specific pokemon, by clicking the close button or pressing ESC or clicking outside of modal the window of Modal will be closed.
+
+
+Used resources:
+Syntaxes: HTML, CSS, JavaScript
+
+
+Source: https://pokeapi.co/api/v2/pokemon
--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,10 @@
-let keys = ["name", "height", "types"];
+
 
 let pokemonRepository = (function(){
     pokemonList = [];
     const loader = document.querySelector("#loading");
-
-   
+    let closeButton = document.querySelector(".close-button");
+    let modal = document.querySelector(".modal-container");
     function add(pokemon){
         pokemonList.push(pokemon);
     }
@@ -65,10 +65,59 @@ let pokemonRepository = (function(){
             console.log(error);
         });
     }
+    /* Show the details of every selected pokemon loaded from API, 
+       showing the details in modal by calling showModal() function
+     */
     function showDetails(pokemon){
         loadDetails(pokemon).then(function(){
-            console.log(pokemon);
+            showModal(pokemon);
         });
+    }
+
+   /*  showModal() function in order to show pokemon details */
+    function showModal(pokemon){
+        
+        let modalTitle = document.querySelector(".modal-header-title");
+        let pokemonImage = document.querySelector(".pokemon-image");
+        let pokemonDetails = document.querySelector(".pokemon-details");
+        let types = "";
+
+        pokemon.types.forEach( function(item){
+            types += item.type.name + " ";
+        });
+
+        modalTitle.innerText = pokemon.name;
+        pokemonImage.src = pokemon.imageUrl;
+        pokemonDetails.innerHTML = "Height: " + pokemon.height 
+                                    + "<br>" + "Types:  " + types ;
+        modal.classList.add("active");
+    }
+    /* In order to close the details Modal there are options:
+       - when the x sign in modal is pressed;
+       - when ESC key is pressed;
+       - when outside of the Modal the pointer is clicked
+       Here are all 3 options are written in order to hide the Modal*/
+         /* 1-Option */
+    closeButton.addEventListener('click', function(){
+       hideModal();
+    });
+       /*  2-option */
+    window.addEventListener('keydown', (e) => {
+        if (e.key === 'Escape' && modal.classList.contains('active')) {
+          hideModal();  
+        }
+    });
+       /*  3-Option */
+    modal.addEventListener('click', (e) => {
+        let target = e.target;
+        if (target === modal) {
+          hideModal();
+        }
+    });
+    
+    /* In order to close or hide the details modal */
+    function hideModal(){
+        modal.classList.remove("active");
     }
     return{
          add: add,
